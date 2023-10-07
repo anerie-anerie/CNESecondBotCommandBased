fd84c680804272fb86db07e06a9093ba71b4dacc
Anerie <141164361+anerie-anerie@users.noreply.github.com>
2023-10-06 23:28:01 -0400

    Add files via upload
    will it work??

---
+++ Constants.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
+    
+  }
+
+  public static class DriveConstants {
+    public static final int DRIVE_AXIS = 1;
+    public static final int TURN_AXIS = 4;
+    public static final int TURN_CONTROLLER_POSITION_TOLERANCE = 1;
+    public static final int TURN_CONTROLLER_VELOCITY_TOLERANCE = 10;
+  }
+
+  public static class IntakeConstants {
+    public static final int INTAKE_OPEN_BUTTON = 2;
+    public static final int INTAKE_CLOSE_BUTTON = 1;
+  }
+}
